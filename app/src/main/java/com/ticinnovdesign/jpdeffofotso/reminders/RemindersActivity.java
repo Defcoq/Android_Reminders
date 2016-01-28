f176df4a6795a0e7a8bffda997d75c59f8a0145b
gallinocoq <defcoq73@yahoo.fr>
2016-01-28 22:16:46 +0100

    Adds a custom icon

--- app/src/main/java/com/ticinnovdesign/jpdeffofotso/reminders/RemindersActivity.java
+++ app/src/main/java/com/ticinnovdesign/jpdeffofotso/reminders/RemindersActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.ActionMode;
@@ -85,8 +86,14 @@ public class RemindersActivity extends AppCompatActivity {
 
 
 
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
