026fa5785e55acf9cdcdfb6d862a7deeb395a7aa
gallinocoq <defcoq73@yahoo.fr>
2016-01-28 21:35:48 +0100

    Adds a ListView dialog for individual list items.

--- app/src/main/java/com/ticinnovdesign/jpdeffofotso/reminders/RemindersActivity.java
+++ app/src/main/java/com/ticinnovdesign/jpdeffofotso/reminders/RemindersActivity.java
@@ -1,17 +1,21 @@
 package com.ticinnovdesign.jpdeffofotso.reminders;
 
+import android.app.Dialog;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.sql.SQLException;
 
@@ -31,7 +35,14 @@ public class RemindersActivity extends AppCompatActivity {
         mDbAdapter = new RemindersDbAdapter(this);
         try {
             mDbAdapter.open();
-            Cursor cursor = mDbAdapter.fetchAllReminders();
+            if (savedInstanceState == null) {
+//Clear all data
+                mDbAdapter.deleteAllReminders();
+//Add some data
+                insertSomeReminders();
+            }
+
+                Cursor cursor = mDbAdapter.fetchAllReminders();
 //from columns defined in the db
             String[] from = new String[]{
                     RemindersDbAdapter.COL_CONTENT
@@ -73,6 +84,57 @@ public class RemindersActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //when we click an individual item in the listview
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(RemindersActivity.this);
+                ListView modeListView = new ListView(RemindersActivity.this);
+                String[] modes = new String[] { "Edit Reminder", "Delete Reminder" };
+                ArrayAdapter<String> modeAdapter = new ArrayAdapter<>(RemindersActivity.this,
+                        android.R.layout.simple_list_item_1, android.R.id.text1, modes);
+                modeListView.setAdapter(modeAdapter);
+                builder.setView(modeListView);
+                final Dialog dialog = builder.create();
+                dialog.show();
+                modeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//edit reminder
+                        if (position == 0) {
+                            Toast.makeText(RemindersActivity.this, "edit "
+                                    + position, Toast.LENGTH_SHORT).show();
+//delete reminder
+                        } else {
+                            Toast.makeText(RemindersActivity.this, "delete "
+                                    + position, Toast.LENGTH_SHORT).show();
+                        }
+                        dialog.dismiss();
+                    }
+                });
+
+                        }
+        });
+
+    }
+
+    private void insertSomeReminders() {
+        mDbAdapter.createReminder("Buy Learn Android Studio", true);
+        mDbAdapter.createReminder("Send Dad birthday gift", false);
+        mDbAdapter.createReminder("Dinner at the Gage on Friday", false);
+        mDbAdapter.createReminder("String squash racket", false);
+        mDbAdapter.createReminder("Shovel and salt walkways", false);
+        mDbAdapter.createReminder("Prepare Advanced Android syllabus", true);
+        mDbAdapter.createReminder("Buy new office chair", false);
+        mDbAdapter.createReminder("Call Auto-body shop for quote", false);
+        mDbAdapter.createReminder("Renew membership to club", false);
+        mDbAdapter.createReminder("Buy new Galaxy Android phone", true);
+        mDbAdapter.createReminder("Sell old Android phone - auction", false);
+        mDbAdapter.createReminder("Buy new paddles for kayaks", false);
+        mDbAdapter.createReminder("Call accountant about tax returns", false);
+        mDbAdapter.createReminder("Buy 300,000 shares of Google", false);
+        mDbAdapter.createReminder("Call the Dalai Lama back", true);
     }
 
     @Override
